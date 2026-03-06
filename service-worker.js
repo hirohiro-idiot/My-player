ccbf80adbefd45e7798b4adc2f939e6e13c50e8b
hirohiro-idiot <yuqiajiujin@gmail.com>
2026-03-06 14:40:33 +0900

    service-worker.js を作成

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "my-player-cache-v1";
+const APP_SHELL = [
+  "./player.html",
+  "./manifest.json",
+  "./IMG_3226.jpeg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  // 同一オリジンのページや静的ファイルはキャッシュ優先
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(req).then((cached) => {
+        if (cached) return cached;
+        return fetch(req).then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+          return res;
+        });
+      })
+    );
+  }
+});
